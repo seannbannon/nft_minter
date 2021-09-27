73c094ca84b768f3f5204c6c8899e23152efb5c4
Sean Bannon <seannbannon@gmail.com>
2021-09-26 21:28:25 -0500

    add wallet listener added

--- nftmint/src/Minter.js
+++ nftmint/src/Minter.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { connectWallet } from "./utils/interact.js";
+import { 
+  connectWallet, 
+  getCurrentWalletConnected,
+} from "./utils/interact.js";
+
 
 
 const Minter = (props) => {
-
-  //State variables
+ //State variables
   const [walletAddress, setWallet] = useState("");
   const [status, setStatus] = useState("");
   const [name, setName] = useState("");
@@ -12,10 +15,40 @@ const Minter = (props) => {
   const [url, setURL] = useState("");
  
   useEffect(async () => { //TODO: implement
-    
+      const {address, status} = await getCurrentWalletConnected();
+      setWallet(address)
+      setStatus(status); 
   }, []);
+  
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  } 
+
+
 
   const connectWalletPressed = async () => { 
+    // 
     const walletResponse = await connectWallet();
     setStatus(walletResponse.status);
     setWallet(walletResponse.address);
